c22037cb37b1a4cf9cc66f61efea30e50edb4717
soloman <solomankirby@yahoo.com>
2022-10-13 17:38:22 +1100

    commit tree 1b29418c15c70dbf6a60670acaf8c04e16098863

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={color: firebrick} >Name is a Web Developer From City</h1>
+    Home</div>;
 }
 
 export default Home;
